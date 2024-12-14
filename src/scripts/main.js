7ae30f201aee81c717d7331be6029af2113d1b65
Yura <yuradziavoruk5@gmail.com>
2024-12-14 14:49:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const rows = [...document.querySelectorAll('tr')];
-const column5 = rows.map((row) => {
-  return row.children[4];
-});
+
 const column2 = rows.map((row) => {
   return row.children[1].cloneNode(true);
 });
 
 for (let i = 0; i < rows.length; i++) {
-  rows[i].appendChild(column2[i]);
-  rows[i].appendChild(column5[i]);
+  rows[i].insertBefore(
+    column2[i],
+    rows[i].children[rows[i].children.length - 1],
+  );
 }
